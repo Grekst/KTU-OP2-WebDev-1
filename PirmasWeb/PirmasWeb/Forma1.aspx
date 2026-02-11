a4233fefab344375ded9dee238b89a266989a08e
Matas <Matas.Kybartas.1@gmail.com>
2026-02-11 07:51:06 +0200

    Add project files.

---
+++ PirmasWeb/PirmasWeb/Forma1.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Forma1.aspx.cs" Inherits="PirmasWeb.Forma1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Vardas"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            &nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" ErrorMessage="Vardas yra privalomas" ForeColor="Red">*</asp:RequiredFieldValidator>
+            <br />
+            <br />
+
+
+            <asp:Label ID="Label2" runat="server" Text="Pavardė"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            &nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="TextBox2" ErrorMessage="Pavardė yra privaloma" ForeColor="Red">*</asp:RequiredFieldValidator>
+            <br />
+            <br />
+
+
+            <asp:Label ID="Label5" runat="server" Text="Amžius"></asp:Label>
+            <br />
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            &nbsp;<asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="DropDownList1" ErrorMessage="Neteisinga metų reikšmė" ForeColor="Red" MaximumValue="25" MinimumValue="14" Type="Integer">*</asp:RangeValidator>
+            <br />
+
+
+
+            <br />
+            <asp:Label ID="Label6" runat="server" Text="Mokamos programavimo kalbos"></asp:Label>
+            <br />
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server" DataSourceID="XmlDataSource2" DataTextField="title" DataValueField="title">
+            </asp:CheckBoxList>
+            <asp:XmlDataSource ID="XmlDataSource2" runat="server" DataFile="~/App_Data/kalbos.xml"></asp:XmlDataSource>
+            <asp:ValidationSummary ID="ValidationSummary1" runat="server" ForeColor="Red" />
+            <br />
+
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Pateikti" Width="109px" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label7" runat="server" Text="Registracijų sąrašas"></asp:Label>
+            <br />
+            <asp:Table ID="Table1" runat="server" BorderStyle="Solid" BorderWidth="1px" Height="172px" Width="688px" GridLines="Both" HorizontalAlign="Justify">
+            </asp:Table>
+            <br />
+            <asp:Button ID="Button2" runat="server" Text="Valyti sąrašą" Width="113px" CausesValidation="False" OnClick="Button2_Click" />
+            <br />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ PirmasWeb/PirmasWeb/Forma1.aspx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PirmasWeb
+{
+    [Serializable]
+    public class UserRegistration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Languages { get; set; }
+    }
+
+    public partial class Forma1 : System.Web.UI.Page
+    {
+        private List<UserRegistration> cachedUserData = new List<UserRegistration>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cachedUserData = new List<UserRegistration>();
+
+            if (DropDownList1.Items.Count == 0)
+            {
+                DropDownList1.Items.Add("-");
+                for (int i = 14; i <= 25; i++)
+                {
+                    DropDownList1.Items.Add(i.ToString());
+                }
+            }
+
+            UpdateTableContents();
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!ClientValidationCheck()) return;
+            if (!ServerValidationCheck()) return;
+
+            AddUserData();
+            Response.Redirect(Request.RawUrl);
+        }
+
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ClearAllUserData();
+            UpdateTableContents();
+        }
+
+        /// <summary>
+        /// Checks if all input fields are filled on the client, before calling ServerValidationCheck method.
+        /// </summary>
+        protected bool ClientValidationCheck()
+        {
+            return Page.IsValid;
+        }
+
+        /// <summary>
+        /// Checks if all client input fields have the correct formatting. (Example: Names cannot have numbers)
+        /// </summary>
+        protected bool ServerValidationCheck()
+        {
+            foreach (char c in TextBox1.Text)
+            {
+                if (!Char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+
+            foreach (char c in TextBox2.Text)
+            {
+                if (!Char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected void AddUserData()
+        {
+            var newUser = new UserRegistration
+            {
+                FirstName = TextBox1.Text,
+                LastName = TextBox2.Text,
+                Age = DropDownList1.SelectedValue,
+                Languages = string.Join(", ", CheckBoxList1.Items.Cast<ListItem>().Where(i => i.Selected).Select(i => i.Value)),
+            };
+
+
+            if (cachedUserData == null) cachedUserData = new List<UserRegistration>();
+            cachedUserData.Add(newUser);
+            SaveUserData();
+            UpdateTableContents();
+        }
+
+        protected void SaveUserData()
+        {
+            Session["SavedUserData"] = cachedUserData;
+        }
+
+        protected void LoadUserData()
+        {
+            var saved = Session["SavedUserData"] as List<UserRegistration>;
+            cachedUserData = saved ?? new List<UserRegistration>();
+        }
+
+        protected void ClearAllUserData()
+        {
+            cachedUserData = new List<UserRegistration>();
+            //Session["SavedUserData"] = null;
+            SaveUserData();
+
+            UpdateTableContents();
+        }
+
+        protected void GenerateTableHeaders()
+        {
+            TableRow row = new TableRow();
+
+            TableCell index = new TableCell();
+            index.Text = "<b>Eilės nr.</b>";
+            row.Cells.Add(index);
+
+            TableCell title = new TableCell();
+            title.Text = "<b>Vardas/Pavardė</b>";
+            row.Cells.Add(title);
+
+            TableCell age = new TableCell();
+            age.Text = "<b>Amžius (metai)</b>";
+            row.Cells.Add(age);
+
+            TableCell programmingLanguages = new TableCell();
+            programmingLanguages.Text = "<b>Kalbos</b>";
+            row.Cells.Add(programmingLanguages);
+
+            Table1.Rows.Add(row);
+
+            Table1.Rows[0].HorizontalAlign = HorizontalAlign.Center;
+        }
+
+        protected void UpdateTableContents()
+        {
+            LoadUserData();
+            Table1.Rows.Clear();
+            GenerateTableHeaders();
+
+            if (cachedUserData == null || cachedUserData.Count == 0) return;
+
+            for (int i = 0; i < cachedUserData.Count; i++)
+            {
+                TableRow newRow = new TableRow();
+
+                newRow.Cells.Add(new TableCell { Text = (i + 1).ToString() }); //index cell
+
+                newRow.Cells.Add(new TableCell { Text = string.Format("{0} {1}", cachedUserData[i].FirstName, cachedUserData[i].LastName) });
+
+                newRow.Cells.Add(new TableCell { Text = cachedUserData[i].Age });
+
+                newRow.Cells.Add(new TableCell { Text = string.IsNullOrEmpty(cachedUserData[i].Languages) ? "-" : cachedUserData[i].Languages });
+
+                Table1.Rows.Add(newRow);
+
+            }
+        }
+    }
+}
